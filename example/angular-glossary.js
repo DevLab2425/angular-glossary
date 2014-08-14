5ee3631fc9b6dfbb07848e82c8000e849fee0fed
devlab2425 <devlab2425@gmail.com>
2014-08-14 17:17:35 -0400

    Example added.

---
+++ example/angular-glossary.js
@@ -0,0 +1,81 @@
+var Glossary = angular.module('angular-glossary', ['template/glossary.html']);
+
+Glossary.directive('glossary', function(){
+	return {
+		scope:{
+			terms: '@'
+		},
+		restrict: 'EA',
+		replace: true,
+		templateUrl: 'template/glossary.html',
+		controller: function ($scope){
+			$scope.createAlphaList = function(terms){
+				var list = [];
+				angular.forEach(terms, function(term){
+					var _alpha = term.term.charAt(0);
+					if(list.indexOf(_alpha) === -1){
+						list.push(_alpha);
+					}
+				});
+				
+				return list.sort();
+			};
+			
+			$scope.selectTerms = function(alpha){
+				$scope.filterAlpha = (alpha) ? alpha.toLowerCase() : undefined;
+			};
+			
+			
+			
+		},
+		controllerAs: 'glossaryCtrl',
+		link: function($scope, iElm, iAttrs, glossaryCtrl) {
+			$scope.alphaList = $scope.createAlphaList(angular.fromJson(iAttrs.terms));
+			$scope.selectedTerms = angular.fromJson(iAttrs.terms);
+		}
+	};
+});
+
+Glossary.filter('startswith', function(){
+	return function(input, alpha){
+		var _out = [];
+
+		if(angular.isUndefined(alpha)){
+			_out = input;
+		}
+		
+		angular.forEach(input, function(item){
+			if(item.term.charAt(0).toLowerCase() === alpha){
+				_out.push(item);
+			}
+		});
+		
+		return _out;
+	};
+});
+
+angular.module('template/glossary.html', [])
+	.run(['$templateCache', function($templateCache){
+		$templateCache.put('template/glossary.html', 
+			'<div class="glossary">' +
+			'<div class="alpha-wrap">' +
+				'<ul class="alpha-list">' +
+					'<li class="alpha-item"><a href ng-click="selectTerms()">All</a></li>' +
+					'<li class="alpha-item" ng-repeat="alpha in alphaList">' +
+						'<a href ng-click="selectTerms(alpha)">{{alpha}}</a>' +
+					'</li>' +
+				'</ul>' +
+			'</div>' +
+			'<div class="definition-wrap">' +
+				'<div class="definition-item">' +
+					'<dl ng-repeat="term in selectedTerms | startswith: filterAlpha | orderBy: \'term\'">' +
+						'<dt>{{term.term}}</dt>' +
+						'<dd>{{term.definition}}</dd>' +
+					'</dl>' +
+				'</div>' +
+			'</div>' +
+		'</div>'
+		);
+	}]);
+	
+	
---
+++ example/example-app.js
@@ -0,0 +1,21 @@
+angular.module('glossaryExample', ['angular-glossary'])
+	.controller('glossaryCtrl', ['$scope', function($scope){
+		$scope.terms = [
+			{
+				term: 'Bananda',
+				definition: 'A long, yellow fruit that monkeys love to eat.'
+			},
+			{
+				term: 'Apple',
+				definition: 'A round, green or red fruit that crunches when you bite it.'
+			},
+			{
+				term: 'Watermelon',
+				definition: 'A very large round fruit that\'s green on the outside and red on the inside.'
+			},
+			{
+				term: 'Grape',
+				definition: 'Small round, red or green fruits on a bunch.'
+			}
+		]
+	}]);
