b72ecb8c293c6291c019402e419f4dbc5959c752
Barry, Paul <paul.barry@contractor.cengage.com>
2014-08-14 17:14:15 -0400

    Initial commit of angular-glossary

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+module.exports = function(grunt){
+	
+	// Load Grunt tasks
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	
+	grunt.initConfig({
+		watch: {
+			karma: {
+				files: ['angular-glossary.js', 'test/{,**/}*.js'],
+				tasks: ['karma:unit']
+			},
+			js: {
+				files: ['angular-glossary.js', 'test/{,**/}*.js', '*.css'],
+				tasks: ['build']
+			}
+		},
+
+		bump: {
+			options: {
+				commitMessage: 'chore: release v%VERSION%',
+				commitFiles: ['package.json', 'bower.json', 'angular-glossary.min.js'],
+				files: ['package.json', 'bower.json'],
+				pushTo: 'origin'
+			}
+		},
+
+		karma: {
+			unit: {
+				configFile: 'karma.conf.js',
+				singleRun: true
+			},
+		},
+
+		jshint: {
+			all: ['Gruntfile.js', 'angular-glossary.js', 'test/angular-glossary.spec.js']
+		},
+
+		ngmin: {
+			dist: {
+				files: {
+					'angular-glossary.min.js': ['angular-glossary.js']
+					
+				}
+			}
+		},
+
+		'npm-contributors': {
+			options: {
+				commitMessage: 'chore: update contributors'
+			}
+		},
+
+		uglify: {
+			options: {
+				mangle: false
+			},
+			dist: {
+				files: {
+					'angular-glossary.min.js': ['angular-glossary.min.js'],
+					'angular-glossary.min.css': ['angular-glossary.min.css']
+				}
+			}
+		},
+		
+		copy: {
+			example: {
+				src: ['angular-glossary.js', 'angular-glossary.css'],
+				dest: 'example/',
+			}
+		}
+		
+	});
+	
+	grunt.registerTask('test', [
+		'jshint:all',
+		'karma:unit'
+	]);
+	
+	grunt.registerTask('build', [
+		'jshint:all',
+		'ngmin',
+		'uglify',
+		'copy:example'
+	]);
+	
+	// Default task
+	grunt.registerTask('default', ['watch']);
+};
--- README.md
+++ README.md
@@ -1,4 +1,48 @@
-angular-glossary
-================
+#angular-glossary
+[![NPM version](https://badge.fury.io/js/angular-glossary.svg)](http://badge.fury.io/js/angular-glossary)
+[![Bower version](https://badge.fury.io/bo/angular-glossary.svg)](http://badge.fury.io/bo/angular-glossary)
+[![Github version](https://badge.fury.io/gh/angular-glossary.svg)](http://badge.fury.io/gh/angular-glossary)
 
 A directive that outputs a glossary based on a collection of terms.
+
+## Installation
+
+Download [angular-glossary.min.js](#) or install with bower
+
+```BASH
+$ bower install angular-glossary --save
+```
+
+Load `angular-glossary.min.js`, then add the `angular-glossary` module to your application.
+
+```javascript
+angular.module('yourApp', ['angular-glossary']);
+```
+
+## Usage
+### HTML
+```html
+
+```
+
+### Javascript
+```javascript
+
+```
+
+### Output
+August 1 - 31, 2014
+
+## Development
+
+Testing is done using Karma Test Runner.
+
+```BASH
+$ grunt test
+```
+	
+## Build
+
+```BASH
+$ grunt build
+```
